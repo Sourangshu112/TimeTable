6b48dcdfbb0681c5fa811c833b8b4eb51c3beb5a
Sourangshu112 <sourangshu098@gmail.com>
2025-12-12 00:04:05 +0530

    Finished adding departments/batches data

---
+++ data.py
@@ -0,0 +1,40 @@
+batches: dict = {
+    #id: [course, yr, dept, room,[interdept split]]
+
+    1 : ["btech", 1, "ME", "C39", ["A"]],
+    2 : ["btech", 1, "CEE", "C39", ["A"]],
+    3 : ["btech", 1, "FET", "C39", ["A"]],
+    4 : ["btech", 1, "EE", "A39", ["A","B"]],
+    5 : ["btech", 1, "CSE", "D39", ["A","B"]],
+
+    6 : ["btech", 2, "ME", "B203", ["A","B"]],
+    7 : ["btech", 2, "CEE", "C25", ["A","B"]],
+    8 : ["btech", 2, "FET", "D25", ["A"]],
+    9 : ["btech", 2, "EE", "A24", ["A","B"]],
+    10: ["btech", 2, "CSE", "C38", ["A","B"]],
+
+    11: ["btech", 3, "ME", "B112", ["A","B"]],
+    12: ["btech", 3, "CEE", "C26", ["A","B"]],
+    13: ["btech", 3, "FET", "D23", ["A"]],
+    14: ["btech", 3, "EE", "A12", ["A","B"]],
+    15: ["btech", 3, "CSE", "C37", ["A"]],
+
+    16: ["btech", 4, "ME", "B110", ["A"]],
+    17: ["btech", 4, "FET", "D22", ["A"]],
+    18: ["btech", 4, "EE", "A13", ["A","B"]],
+
+    19: ["diploma", 1, "EE+ME+FPT", "D38", ["A","B"]],
+    20: ["diploma", 1, "CST+CE", "C24", ["A","B"]],
+
+    21: ["diploma", 2, "ME", "B204", ["A"]],
+    22: ["diploma", 2, "CE", "C22", ["A"]],
+    23: ["diploma", 2, "FET", "D12", ["A"]],
+    24: ["diploma", 2, "EE", "A35", ["A"]],
+    25: ["diploma", 2, "CST", "D26", ["A"]],
+
+    26: ["diploma", 3, "ME", "B202", ["A"]],
+    27: ["diploma", 3, "CE", "C23", ["A"]],
+    28: ["diploma", 3, "FET", "D24", ["A"]],
+    29: ["diploma", 3, "EE", "A36", ["A"]],
+    30: ["diploma", 3, "CST", "D37", ["A"]],
+}
